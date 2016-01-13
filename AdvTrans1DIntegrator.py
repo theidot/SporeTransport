f344ead0e568988bdcfe7e9f811a309e773612e5
Dave Schneider <Dave.Schneider@ars.usda.gov>
2016-01-13 16:08:54 -0500

    Added unit tests and fixed bugs found by same.

--- AdvTrans1DIntegrator.py
+++ AdvTrans1DIntegrator.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 
 class Integrator(object):
 
-    def __init__(self, xgrid):
+    def __init__(self, pde):
         self.init_graphics()
-        self.__x = np.copy(xgrid)
+        self.__pde = pde 
         
     def init_graphics(self):
         self.fig = plt.figure()
@@ -16,26 +16,47 @@ class Integrator(object):
     def grid(self):
         return np.copy(self.__x)
 
-    def delta_plus(self, u):
-        return np.roll(u, -1) - u
+    def delta_plus(self, u, order=1):
+        if order == 1:
+            return np.roll(u, -1) - u
+        elif order == 2:
+            return 0.5*(-np.roll(u, -2) + 4.0*np.roll(u, -1) - 3.0*u)
+        elif order == 3:
+            return (1.0/6.0)*(-np.roll(u, -2) + 6.0*np.roll(u, -1) - 3.0*u - 2.0*np.roll(u, 1))
+        else:
+            raise NotImplementedError('Difference scheme of order {0} for delta_plus() not implemented'.format(order))
 
-    def delta_zero(self, u):
-        return np.roll(u, -1) - np.roll(u, 1)
+    def delta_zero(self, u, order=1):
+        if order == 1:
+            return np.roll(u, -1) - np.roll(u, 1)
+        else:
+            raise NotImplementedError('Difference scheme of order {0} for delta_zero() not implemented'.format(order))
 
-    def delta_minus(self, u):
-        return u - np.roll(u, 1)
+    def delta_minus(self, u, order=1):
+        if order == 1:
+            return u - np.roll(u, 1)
+        elif order == 2:
+            return 0.5*(3.0*u - 4.0*np.roll(u, 1) + 3.0*np.roll(u, 2))
+        elif order == 3:
+            return (1.0/6.0)*(2.0*np.roll(u, -1) + 3.0*u - 6.0*roll(u, 1) + np.roll(u, 2))
+        else:
+            raise NotImplementedError('Difference scheme of order {0} for delta_minus() not implemented'.format(order))
 
-    def delta_square(self, u):
-        return np.roll(u, -1) - 2.0*u + np.roll(u, 1)
+    def delta_square(self, u, order=2):
+        if order == 2:
+            return np.roll(u, -1) - 2.0*u + np.roll(u, 1)
+        else:
+            raise NotImplementedError('Difference scheme of order {0} for delta_square() not implemented'.format(order))
 
-    def update_graphics(self, u, t, w):
+    def update_graphics(self, t, w=None):
 
         #
         # Update plot and pause
         #
         plt.ylim([-0.1, 1.1])
-        plt.plot(self.__x, u, 'k')
-        plt.plot(self.__x, w, 'r')            
+        plt.plot(self.__pde.grid(), self.__pde.density(), 'k')
+        if w is not None:
+            plt.plot(self.__pde.grid(), w, 'r')            
         plt.title('t={0:.4f}'.format(t))
 
     def pause(self, viewing_delay= 0.01):
